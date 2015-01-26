a007c477fb7b9c53e545fc8fe4ca86c7f21d10ef
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-26 11:32:17 -0500

    Documented hero class

--- src/Hero.java
+++ src/Hero.java
@@ -4,7 +4,7 @@
 
 /**
  * @author Viral
- *@version 1.1
+ *@version 1.2
  */
 public class Hero {
 	
@@ -27,10 +27,13 @@ public class Hero {
 	
 	//Public methods------------------------------------------------------
 	
+	
+	//this method will call hitAttempt method
 	public void fight() {
 		hitAttempt();
 	}
 	
+	//this method will show hero's abilities
 	public void show() {
 		System.out.println("Strength:" + this.strength);
 		System.out.println("Speed:" + this.speed);
@@ -39,6 +42,7 @@ public class Hero {
 	
 	//private methods----------------------------------------------------
 	
+	//this methods will generate hero's abilities
 	private void generateAbilities() {
 		
 		this.strength = (int)(Math.random()*100+1);
@@ -46,9 +50,10 @@ public class Hero {
 		this.health = (int)(Math.random()*100+1);
 	}
 	
+	//this method will determine is the hero's been hit or no
 	private boolean hitAttempt() {
 		int check = (int)(Math.random()*100+1);
-		if(check > 1 && check < 21) {
+		if(check > 1 && check < 21) { //to make it 20% chance of hit
 			System.out.println("Hero has been hit.");
 			hitDamage();
 			return true;
@@ -58,6 +63,7 @@ public class Hero {
 		}
 	}
 	
+	//if the hero's been hit, this method will calculate hero's damage
 	private int hitDamage() {
 		int damage = strength * (int)(Math.random()*6+1);
 		System.out.println("The damage is " + damage);
